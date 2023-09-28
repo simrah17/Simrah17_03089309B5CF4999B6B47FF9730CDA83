72c694bd4e2df576a6774fadd08d3b51a567ce24
simrah17 <144542432+simrah17@users.noreply.github.com>
2023-09-28 16:02:36 +0530

    Add files via upload

---
+++ challenges unit-3/main.py
@@ -0,0 +1,13 @@
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
+
+# Example usage:
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
+target_product = "Apple"
+
+result = linear_search_product(products, target_product) 
+print(result)
